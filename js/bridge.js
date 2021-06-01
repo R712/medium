6773a6222d670823213fce688516e5cb1bfdb101
Ahreum Han <R712@users.noreply.github.com>
2021-06-01 11:18:09 +0900

    1차 피드백 반영 및 2차
    주로 모달과 지갑 연결 전과 후 상태를 보시면 됩니다.

--- js/bridge.js
+++ js/bridge.js
@@ -1,17 +1,46 @@
 const btnChooseCoin = document.querySelector('#btn-choose-coin');
 const modalChooseCoin = document.querySelector('#modal-choose-coin');
-const btnModalClose = document.querySelector('.btn-clear');
-const modals = document.querySelector('.modal');
-const modalOverlay = document.querySelector('.modal-overlay');
+const btnConnectWallet = document.querySelector('#btn-connect-wallet');
+const modalConnectWallet = document.querySelector('#modal-connect-wallet');
+const modalWalletDone = document.querySelector('#modal-wallet-done');
+const btnModalClose = document.querySelectorAll('.btn-clear');
+const modals = document.querySelectorAll('.modal');
+const modalOverlay = document.querySelectorAll('.modal-overlay');
 
 btnChooseCoin.addEventListener('click', () => {
-    modalChooseCoin.classList.toggle('active');
+    modalChooseCoin.classList.toggle('modal-show');
 });
 
-btnModalClose.addEventListener('click', () => {
-    modals.classList.remove('active');
+btnConnectWallet.addEventListener('click', () => {
+    modalConnectWallet.classList.toggle('modal-show');
 });
 
-modalOverlay.addEventListener('click', () => {
-    modals.classList.remove('active');
+btnModalClose.forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelector('.modal-show').classList.remove('modal-show');
+    });
+});
+
+modalOverlay.forEach(a => {
+    a.addEventListener('click', () => {
+        document.querySelector('.modal-show').classList.remove('modal-show');
+    });
+});
+
+document.querySelector('#btn-wallet-done').addEventListener('click', () => {
+    modalConnectWallet.classList.toggle('modal-show');
+    modalWalletDone.classList.toggle('modal-show');
+});
+
+
+let bgThemeVal;
+
+const bgRadios = document.querySelectorAll('input[name=bg]');
+const bgBody = document.querySelector('body');
+bgRadios.forEach(radio => {
+    radio.addEventListener('click', function (el) {
+        bgThemeVal = radio.value;
+        bgBody.removeAttribute('class');
+        bgBody.classList.add(bgThemeVal);
+    });
 });
